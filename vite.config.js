fc4a2c29076ee9acdb9342dd6d07d12cfbe98ff7
Palak <palakbansal.tech@gmail.com>
2026-04-16 19:48:28 +0530

    fix(): added changes to vite config

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5173,
+    port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
+      "/api": {
+        target: "https://pglife-backend.onrender.com",
         changeOrigin: true,
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
